0a3dba099a931e5fec8164f3dcb2808df01df8d0
Rajasa Savant <rasavant@microsoft.com>
2018-06-27 09:40:28 -0700

    Azure Function to get IoTHub device details

---
+++ GetIoTHubDataFunction/GetIoTHubDataFunction/DeviceDetails.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GetIoTHubDataFunction
+{
+    class DeviceDetails
+    {
+        public DeviceConnectionState ConnectionState { get; set; }
+        public string DeviceId { get; set; }
+        public string ETag { get; set; }
+        public DeviceStatus Status { get; set; }
+        public long Version { get; set; }
+        public System.DateTime StatusUpdatedTime { get; set; }
+        public TwinProperties Properties { get; set; }
+        public System.DateTime LastActivityTime { get; set; }
+    }
+}
---
+++ GetIoTHubDataFunction/GetIoTHubDataFunction/GetDeviceData.cs
@@ -0,0 +1,55 @@
+
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+using Microsoft.Azure.Devices;
+using System.Collections.Generic;
+using System;
+
+namespace GetIoTHubDataFunction
+{
+    public static class GetDeviceData
+    {
+        [FunctionName("GetDeviceData")]
+        public static async System.Threading.Tasks.Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+
+            string connectionUri = "HostName=reactorhackrj0d722.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=cdzkozHgpDB08H5U2xQZN/0taN5LTDCTTVjaTDTBCAM=";//"HostName=iothub-c7wdu.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=Ml471les8qtgRdm4MCTKEeBBonEN5u0n0SOIpEiaApo=";
+            RegistryManager registryManager;
+            registryManager = RegistryManager.CreateFromConnectionString(connectionUri);
+            var query = registryManager.CreateQuery("SELECT * FROM devices", 100);
+            List<DeviceDetails> deviceDetails = new List<DeviceDetails>();
+
+            while (query.HasMoreResults)
+            {
+                var page = await query.GetNextAsTwinAsync();
+                foreach (var twin in page)
+                {
+                    DeviceDetails device = new DeviceDetails();
+                    device.ConnectionState = (DeviceConnectionState)twin.ConnectionState;
+                    device.DeviceId = twin.DeviceId;
+                    device.ETag = twin.ETag;
+                    device.Status = (DeviceStatus)twin.Status;
+                    device.Version = (long)twin.Version;
+                    device.StatusUpdatedTime = (System.DateTime)twin.StatusUpdatedTime;
+                    device.Properties = twin.Properties;
+                    device.LastActivityTime = (System.DateTime)twin.LastActivityTime;
+
+                    deviceDetails.Add(device);
+                    Console.WriteLine(twin.DeviceId.ToString());
+                }
+            }
+
+            string sendData = JsonConvert.SerializeObject(deviceDetails);
+
+            return sendData != null
+                ? (ActionResult)new OkObjectResult($"{sendData}")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
+    }
+}
